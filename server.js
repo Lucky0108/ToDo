03b23014d488b2113f5af7d47adcbf0dd140d598
Lakshay Yadav <52318871+Lucky0108@users.noreply.github.com>
2022-08-24 13:34:39 +0530

    Updated listening console

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const db = config.get("mongoURI");
 
 mongoose.connect(db, {useUnifiedTopology: true, useNewUrlParser:true})
-        .then(() => console.log("MongoDB Database is Connected..."))
+        .then(() => console.log("MongoDB Database is Connected With jenkin server now..."))
         .catch(err => console.log(err));
 
 // Use Routes
